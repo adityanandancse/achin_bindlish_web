3813c9a63405d95118422c82f2e53924978217db
Aditya Nandan <cse.adityanandan@gmail.com>
2025-01-22 16:21:59 +0530

    File uploaded

---
+++ handler.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['Name','Email'])->areRequired()->maxLength(50);
+$validator->field('Email')->isEmail();
+$validator->field('Message')->maxLength(6000);
+
+
+$pp->requireReCaptcha();
+$pp->getReCaptcha()->initSecretKey('6LdNp7EqAAAAANDF-w8NGerdOgGD4bgwe1kEMh2d');
+
+
+$pp->sendEmailTo('hello@achinbindlish.com'); // ← Your email here
+
+echo $pp->process($_POST);
